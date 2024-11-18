16da7ee8762a9f5d7a9a33d8e3699b67882b8fed
Kanarious <keven.fletes339@gmail.com>
2024-11-18 18:09:00 -0500

    change port to 3000 from 80

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:80', //local Nest app
+        target: 'http://localhost:3000', //local Nest app
         changeOrigin: true
       }
     }
